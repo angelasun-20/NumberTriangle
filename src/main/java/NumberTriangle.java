bcd5f74fa0404fd37eb0d77299caf3a30727e666
angelasun20 <angelamango20@gmail.com>
2025-09-26 22:35:28 -0400

    implemented retrieve method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -113,8 +113,16 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        if (path == null || path.isEmpty()) {
+            return root;
+        }
+        char c = path.charAt(0);
+        String rest = path.substring(1);
+        if (c == 'l') {
+            return left.retrieve(rest);
+        } else {
+            return right.retrieve(rest);
+        }
     }
 
     /** Read in the NumberTriangle structure from a file.
